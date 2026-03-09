27d8f96901e1209b971444e69df05648d15d9ad9
Shanto93 <shantoislam7363@gmail.com>
2026-03-09 16:57:14 +0600

    Install and all setup done for prisma and also successfully connected with database

--- health-care-server/src/app.ts
+++ health-care-server/src/app.ts
@@ -1,25 +1,29 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import notFound from './app/middlewares/notFound';
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+import config from "./config";
 
 const app: Application = express();
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5000"],
+    credentials: true,
+  }),
+);
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Server is running ...",
+    Time: new Date().toLocaleTimeString(),
+    UpTime: process.uptime().toFixed(2) + " seconds",
+    Environment: config.node_env,
+  });
 });
-
 
 app.use(globalErrorHandler);
 
