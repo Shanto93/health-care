30654772d3df0432d665ec45350545d576c4bd9c
Shanto93 <shantoislam7363@gmail.com>
2026-04-12 00:10:12 +0600

    Fix error in auth.ts file

--- health-care-server/src/app.ts
+++ health-care-server/src/app.ts
@@ -1,4 +1,4 @@
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser"; // 1. Import cookie parser
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
@@ -7,6 +7,7 @@ import router from "./app/routes";
 import config from "./config";
 
 const app: Application = express();
+
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:5000"],
@@ -14,10 +15,14 @@ app.use(
   }),
 );
 
-//parser
-app.use(express.json());
+// 2. Initialize cookie parser before your routes
 app.use(cookieParser());
+
+// parsers
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// routes
 app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
@@ -30,7 +35,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
-
 app.use(notFound);
 
 export default app;
--- health-care-server/src/app/middlewares/auth.ts
+++ health-care-server/src/app/middlewares/auth.ts
@@ -8,15 +8,16 @@ const auth = (...roles: string[]) => {
     res: Response,
     next: NextFunction,
   ) => {
-    // 1. Get token from header
     try {
-      const token = req.cookies.get.accessToken || req.headers.authorization?.split(" ")[1];
+      // 1. Get token from header or cookies
+      const token =
+        req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
 
       if (!token) {
         throw new Error("Unauthorized: No token provided");
       }
-      // 2. Verify token
 
+      // 2. Verify token
       const verifyUser = verifyToken(
         token,
         config.jwt.access_token_secret as string,
@@ -26,6 +27,7 @@ const auth = (...roles: string[]) => {
         throw new Error("Unauthorized: Invalid token");
       }
 
+      // Attach the verified user to the request object
       req.user = verifyUser;
 
       // 3. Check if user role is allowed
