dfa45dca6cc91d53efd658027c292067dc18ba75
Shanto93 <shantoislam7363@gmail.com>
2026-04-13 13:53:26 +0600

    Modified some fixes in globalErrorHandler, notFound and validateRequest middleware

--- health-care-server/src/app/middlewares/globalErrorHandler.ts
+++ health-care-server/src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,23 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { NextFunction, Request, Response } from "express";
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+const globalErrorHandler = async (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const httpStatus = await import("http-status");
 
-    let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
-    let success = false;
-    let message = err.message || "Something went wrong!";
-    let error = err;
+  let statusCode = httpStatus.default.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message || "Something went wrong!";
+  let error = err;
 
-    res.status(statusCode).json({
-        success,
-        message,
-        error
-    })
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
+  });
 };
 
 export default globalErrorHandler;
--- health-care-server/src/app/middlewares/notFound.ts
+++ health-care-server/src/app/middlewares/notFound.ts
@@ -1,15 +1,16 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { NextFunction, Request, Response } from "express";
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.NOT_FOUND).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!"
-        }
-    })
+  import("http-status").then((httpStatus) => {
+    res.status(httpStatus.default.NOT_FOUND).json({
+      success: false,
+      message: "API NOT FOUND!",
+      error: {
+        path: req.originalUrl,
+        message: "Your requested path is not found!",
+      },
+    });
+  });
 };
 
 export default notFound;
